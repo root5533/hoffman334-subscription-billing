f9037240315bb82045e9093822e1812dae2ca200
root5533 <ymuraad@gmail.com>
2018-09-08 15:54:11 +0530

    Design home page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Subscription-Choices';
+  title = 'Subscription Choices';
 }
